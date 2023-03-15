c359b9c05a8d1ef6f98931a23d4783e2cc49475f
Aleksandr Grigoryev <AleksandrGrigoryev@itcenterifmo.onmicrosoft.com>
2023-03-15 21:03:11 +0300

    Lab 4 part 1 add main

---
+++ src/Lab4/Part1/Main.java
@@ -0,0 +1,10 @@
+package Lab4.Part1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cycles.main(args);
+        CalcChecking.main(args);
+        ArrayChecking.main(args);
+
+    }
+}
