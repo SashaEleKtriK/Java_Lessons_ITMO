ae56c2576e4d71ce5c4d821c39bdf79c412f443d
Aleksandr Grigoryev <AleksandrGrigoryev@itcenterifmo.onmicrosoft.com>
2023-03-16 14:57:01 +0300

    Lab 4 part 1 ArrayChecking methods change

--- src/Lab4/Part1/ArrayChecking.java
+++ src/Lab4/Part1/ArrayChecking.java
@@ -4,35 +4,32 @@ import java.util.Arrays;
 
 public class ArrayChecking {
     public static void main(String[] args) {
-        exercise5Test();
-        exercise6Test();
+        int[] ints1 = {1, 2, 3, 4, 88, 3, 77};
+        int[] ints2 = {3, 2, 2};
+        int[] ints3 = {3};
+        exercise5Test(ints1, ints2, ints3);
+        exercise6Test(ints1, ints2, ints3);
     }
 
-    public static void exercise5Test() {
+    public static void exercise5Test(int[] ints1, int[] ints2, int[] ints3) {
         System.out.println("============");
         System.out.println("Задание 5");
         System.out.println("Появляется ли число 3 как первый или последний элемент массива целых чисел.");
         System.out.println("============");
-        int[] ints1 = {1, 2, 3, 4, 88, 3, 77};
         firstArray(ints1);
-        int[] ints2 = {3, 2, 2};
         firstArray(ints2);
-        int[] ints3 = {3};
         firstArray(ints3);
         System.out.println("============");
         System.out.println("************");
     }
 
-    public static void exercise6Test() {
+    public static void exercise6Test(int[] ints1, int[] ints2, int[] ints3) {
         System.out.println("============");
         System.out.println("Задание 6");
         System.out.println("Проверить, что массив содержит число 1 или 3. ");
         System.out.println("============");
-        int[] ints1 = {1, 2, 3, 4, 88, 3, 77};
         secondArray(ints1);
-        int[] ints2 = {3, 2, 2};
         secondArray(ints2);
-        int[] ints3 = {4, 67, 87};
         secondArray(ints3);
         System.out.println("============");
         System.out.println("************");
